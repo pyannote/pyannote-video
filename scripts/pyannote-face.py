8ca3a4098df09a24667612e334e32a48e91f0682
Hervé BREDIN <bredin@limsi.fr>
2015-10-19 14:36:10 +0200

    feat: tracking and facial features detection

--- scripts/pyannote-face.py
+++ scripts/pyannote-face.py
@@ -29,14 +29,14 @@
 """Face detection and tracking
 
 Usage:
-  pyannote-face track [options] <video> <shot> <face> <track>
-  pyannote-face detect [options] <video> <detection>
+  pyannote-face detect [--verbose] [options] <video> <output>
+  pyannote-face track [--verbose] <video> <shot> <detection> <output>
+  pyannote-face shape [--verbose] <video> <model> <tracking> <output>
   pyannote-face (-h | --help)
   pyannote-face --version
 
 Options:
   --every=<msec>       Process one frame every <msec> milliseconds.
-  --shape=<model>      Perform facial features detection using <model>.
   --smallest=<size>    (approximate) size of smallest face [default: 36].
   -h --help            Show this screen.
   --version            Show version.
@@ -48,6 +48,7 @@ from pyannote.video import __version__
 from pyannote.video import Video
 
 from tqdm import tqdm
+from munkres import Munkres
 import numpy as np
 import cv2
 
@@ -55,11 +56,83 @@ import dlib
 
 SMALLEST_DEFAULT = 36
 
+FACE_TEMPLATE = ('{t:.3f} {identifier:d} '
+                 '{left:d} {top:d} {right:d} {bottom:d} '
+                 '{confidence:.3f}\n')
 
-def detect(video, output,
-           step=None, shape=None,
-           upscale=1,
-           show_progress=False):
+
+def getShotGenerator(shotFile):
+    """Parse precomputed shot file and generate boundary timestamps"""
+
+    t = yield
+
+    with open(shotFile, 'r') as f:
+
+        for line in f:
+            T = float(line.strip())
+
+            while True:
+                # loop until a large enough t is sent to the generator
+                if T > t:
+                    t = yield
+                    continue
+
+                # else, we found a new shot
+                t = yield T
+                break
+
+
+def getFaceGenerator(detection):
+    """Parse precomputed face file and generate timestamped faces"""
+
+    # t is the time sent by the frame generator
+    t = yield
+
+    with open(detection, 'r') as f:
+
+        faces = []
+        currentT = None
+
+        for line in f:
+
+            # parse line
+            # time, identifier, left, top, right, bottom, confidence
+            tokens = line.strip().split()
+            T = float(tokens[0])
+            identifier = int(tokens[1])
+            face = dlib.drectangle(*[int(token) for token in tokens[2:6]])
+
+            # load all faces from current frame
+            # and only those faces
+            if T == currentT or currentT is None:
+                faces.append((identifier, face))
+                currentT = T
+                continue
+
+            # once all faces at current time are loaded
+            # wait until t reaches current time
+            # then returns all faces at once
+
+            while True:
+
+                # wait...
+                if currentT > t:
+                    t = yield t, []
+                    continue
+
+                # return all faces at once
+                t = yield currentT, faces
+
+                # reset current time and corresponding faces
+                faces = [(identifier, face)]
+                currentT = T
+                break
+
+        while True:
+            t = yield t, []
+
+
+def detect(video, output, step=None, upscale=1, show_progress=False):
     """Face detection"""
 
     # frame iterator
@@ -79,13 +152,9 @@ def detect(video, output,
     # face detector
     faceDetector = dlib.get_frontal_face_detector()
 
-    # facial features detector
-    if shape is None:
-        facialFeaturesDetector = None
-    else:
-        facialFeaturesDetector = dlib.shape_predictor(shape)
+    identifier = 0
 
-    with open(output, 'w') as f:
+    with open(output, 'w') as foutput:
 
         for t, frame in generator:
 
@@ -93,23 +162,178 @@ def detect(video, output,
 
             for boundingBox in faceDetector(gray, upscale):
 
-                f.write('{t:.3f} {left:d} {top:d} {right:d} {bottom:d}'.format(
-                    t=t, left=boundingBox.left(), right=boundingBox.right(),
+                identifier = identifier + 1
+
+                foutput.write(FACE_TEMPLATE.format(
+                    t=t, identifier=identifier, confidence=-1,
+                    left=boundingBox.left(), right=boundingBox.right(),
                     top=boundingBox.top(), bottom=boundingBox.bottom()))
 
-                if facialFeaturesDetector:
 
-                    points = facialFeaturesDetector(gray, boundingBox)
-                    facialFeatures = [(p.x, p.y) for p in points.parts()]
+def track(video, shot, detection, output, show_progress=False):
+    """Tracking by detection"""
 
-                    for x, y in facialFeatures:
-                        f.write(' {x:d} {y:d}'.format(x=x, y=y))
+    # frame generator
+    frames = video.iterframes(with_time=True)
+    if show_progress:
+        frames = tqdm(iterable=frames,
+                      total=video.duration * video.fps,
+                      leave=True, mininterval=1.,
+                      unit='frames', unit_scale=True)
 
-                f.write('\n')
-                f.flush()
+    # shot generator
+    shotGenerator = getShotGenerator(shot)
+    shotGenerator.send(None)
 
-def track(video):
-    pass
+    # face generator
+    faceGenerator = getFaceGenerator(detection)
+    faceGenerator.send(None)
+
+    # Hungarian algorithm for face/tracker matching
+    hungarian = Munkres()
+
+    trackers = dict()
+    confidences = dict()
+    identifier = 0
+
+    with open(output, 'w') as foutput:
+
+        for timestamp, frame in frames:
+
+            gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+            shot = shotGenerator.send(timestamp)
+
+            # reset trackers at shot boundaries
+            if shot:
+                trackers.clear()
+                confidences.clear()
+
+            # get all detected faces at this time
+            T, faces = faceGenerator.send(timestamp)
+            # not that T might be differ slightly from t
+            # due to different steps in frame iteration
+
+            # update all trackers and store their confidence
+            for i, tracker in trackers.items():
+                confidences[i] = tracker.update(gray)
+
+            unmatched = set(trackers)
+
+            Nt, Nf = len(trackers), len(faces)
+            if Nt and Nf:
+
+                # compute intersection for every tracker/face pair
+                N = max(Nt, Nf)
+                areas = np.zeros((N, N))
+                trackers_ = trackers.items()
+                for t, (i, tracker) in enumerate(trackers_):
+                    position = tracker.get_position()
+                    for f, (_, face) in enumerate(faces):
+                        areas[t, f] = position.intersect(face).area()
+
+                # find the best one-to-one mapping
+                mapping = hungarian.compute(np.max(areas) - areas)
+
+                for t, f in mapping:
+
+                    if t >= Nt or f >= Nf:
+                        continue
+
+                    area = areas[t, f]
+
+                    _, face = faces[f]
+                    faceArea = face.area()
+
+                    i, tracker = trackers_[t]
+                    trackerArea = tracker.get_position().area()
+
+                    # if enough overlap,
+                    # re-intialize tracker and mark face as matched
+                    if (2 * area > faceArea) or (2 * area > trackerArea):
+                        tracker.start_track(gray, face)
+                        unmatched.remove(i)
+
+                        foutput.write(FACE_TEMPLATE.format(
+                            t=T, identifier=i, confidence=confidences[i],
+                            left=int(face.left()), right=int(face.right()),
+                            top=int(face.top()), bottom=int(face.bottom())))
+
+                        faces[f] = None, None
+
+            for _, face in faces:
+
+                # this face was matched already
+                if face is None:
+                    continue
+
+                # new tracker
+                identifier = identifier + 1
+                tracker = dlib.correlation_tracker()
+                tracker.start_track(gray, face)
+                confidences[identifier] = tracker.update(gray)
+                trackers[identifier] = tracker
+
+                foutput.write(FACE_TEMPLATE.format(
+                    t=T, identifier=identifier, confidence=0.000,
+                    left=int(face.left()), right=int(face.right()),
+                    top=int(face.top()), bottom=int(face.bottom())))
+
+            for i, tracker in trackers.items():
+
+                if i not in unmatched:
+                    continue
+
+                face = tracker.get_position()
+
+                foutput.write(FACE_TEMPLATE.format(
+                    t=T, identifier=i, confidence=confidences[i],
+                    left=int(face.left()), right=int(face.right()),
+                    top=int(face.top()), bottom=int(face.bottom())))
+
+
+def shape(video, model, tracking, output, show_progress=False):
+    """Facial features detection"""
+
+    # frame generator
+    frames = video.iterframes(with_time=True)
+    if show_progress:
+        frames = tqdm(iterable=frames,
+                      total=video.duration * video.fps,
+                      leave=True, mininterval=1.,
+                      unit='frames', unit_scale=True)
+
+    # face generator
+    faceGenerator = getFaceGenerator(tracking)
+    faceGenerator.send(None)
+
+    # facial features detector
+    facialFeaturesDetector = dlib.shape_predictor(model)
+
+    with open(output, 'w') as foutput:
+
+        for timestamp, frame in frames:
+
+            gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+            # get all detected faces at this time
+            T, faces = faceGenerator.send(timestamp)
+            # not that T might be differ slightly from t
+            # due to different steps in frame iteration
+
+            for identifier, face in faces:
+
+                boundingBox = dlib.rectangle(
+                    int(face.left()), int(face.top()),
+                    int(face.right()), int(face.bottom()))
+                points = facialFeaturesDetector(gray, boundingBox)
+                facialFeatures = [(p.x, p.y) for p in points.parts()]
+
+                foutput.write('{t:3f} {identifier:d}'.format(
+                    t=T, identifier=identifier))
+                for x, y in facialFeatures:
+                    foutput.write(' {x:d} {y:d}'.format(x=x, y=y))
+                foutput.write('\n')
 
 
 if __name__ == '__main__':
@@ -119,23 +343,19 @@ if __name__ == '__main__':
     arguments = docopt(__doc__, version=version)
 
     # initialize video
-    filename = arguments['<video>']
-    video = Video(filename)
+    video = Video(arguments['<video>'])
 
     verbose = arguments['--verbose']
 
+    # face detection
     if arguments['detect']:
 
+        # every xxx milliseconds
         every = arguments['--every']
         if not every:
             step = None
         else:
             step = 1e-3 * float(arguments['--every'])
-
-        # facial features detection
-        shape = arguments['--shape']
-        if not shape:
-            shape = None
 
         # (approximate) size of smallest face
         smallest = float(arguments['--smallest'])
@@ -144,12 +364,26 @@ if __name__ == '__main__':
         else:
             upscale = int(np.ceil(SMALLEST_DEFAULT / smallest))
 
-        detection = arguments['<detection>']
+        output = arguments['<output>']
 
-        detect(video, detection,
-               step=step, shape=shape,
-               upscale=upscale,
+        detect(video, output,
+               step=step, upscale=upscale,
                show_progress=verbose)
 
+    # face tracking
     if arguments['track']:
-        track(video, show_progress=verbose)
+
+        shot = arguments['<shot>']
+        detection = arguments['<detection>']
+        output = arguments['<output>']
+        track(video, shot, detection, output,
+              show_progress=verbose)
+
+    # facial features detection
+    if arguments['shape']:
+
+        tracking = arguments['<tracking>']
+        model = arguments['<model>']
+        output = arguments['<output>']
+        shape(video, model, tracking, output,
+              show_progress=verbose)
